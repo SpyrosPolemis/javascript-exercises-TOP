59f0f210fcb20ef47bd8e70f786d2060fcb4dbdc
SpyrosPolemis <spy.polemis@gmail.com>
2025-06-05 18:11:50 +1000

    Complete task 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, timesToRepeat) {
+    if (timesToRepeat < 0) return "ERROR";
+    let repeatedString = "";
+    for (let i = 0; i < timesToRepeat; i++) {
+        repeatedString += stringToRepeat;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
