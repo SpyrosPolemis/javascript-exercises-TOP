9723a9b87c8be9e13c68c2334ad13427eb98a9ce
SpyrosPolemis <spy.polemis@gmail.com>
2025-06-05 19:44:31 +1000

    Complete task 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (number1 < 0 || number2 < 0) return "ERROR"
+    if (!Number.isInteger(number1) || !Number.isInteger(number2)) {
+        return "ERROR";
+    }
 
+    if (number2 > number1) [number1, number2] = [number2, number1]
+
+    if (number1 - number2 > 0) {
+        max = number1
+        min = number2
+    } else {
+        max = number2
+        min = number1
+    }
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+    return sum;
 };
 
 // Do not edit below this line
